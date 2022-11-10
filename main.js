bbd1247a012a62056e625f2d471224979e10075d
jake-hahne <113376744+jake-hahne@users.noreply.github.com>
2022-11-09 18:39:03 -0600

    changed the scripting to add an empty list message (placeholder). will develop further

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
  * Written by Jake Hahne for Web Dev semester project at UNK
  * Fall 2022
  */
+
 window.addEventListener('load', () => {
 	const form = document.querySelector("#new-task-form");
 	const input = document.querySelector("#new-task-input");
@@ -9,89 +10,111 @@ window.addEventListener('load', () => {
 
 	/****************************************************************************************************************
 	 This section of the script has the purpose of loading all list items by looping through the browsers local storage
-	 and creating the task elements and insjecting them into the HTML code for the web page. This process happens on
+	 and creating the task elements and injecting them into the HTML code for the web page. This process happens on
 	 page load, therefore when list items are made, they will not be lost on refresh or even quitting the browser. They
 	 will only be removed from local storage when they are explicitly deleted by the user.
 	 *****************************************************************************************************************/
 	const arrayOfTodos = Object.keys(localStorage);
-	for (let i = 0; i < arrayOfTodos.length; i++){
+	const emptyMessage = "Would you look at that, you have nothing to do!";
 
-		//--------------------- Container for a 'task' ------------------------//
+	if (localStorage.length > 0) {
+		for (let i = 0; i < arrayOfTodos.length; i++) {
+
+			//--------------------- Container for a 'task' ------------------------//
+			const task_el = document.createElement('div');
+			task_el.classList.add('task');
+
+			// Checkmark for each task
+			const task_check = document.createElement('input');
+			task_check.type = 'checkbox'
+
+			const task_content_el = document.createElement('div');
+			task_content_el.classList.add('content');
+
+			task_el.appendChild(task_check);
+			task_el.appendChild(task_content_el);
+
+			// Text box used to display a task, can be edited after clicking edit
+			const task_input_el = document.createElement('input');
+			task_input_el.classList.add('text');
+			task_input_el.type = 'text';
+			task_input_el.value = arrayOfTodos[i];
+			task_input_el.setAttribute('readonly', 'readonly');
+
+			task_content_el.appendChild(task_input_el);
+
+			// Container for edit and delete buttons
+			const task_actions_el = document.createElement('div');
+			task_actions_el.classList.add('actions');
+
+			// Edit button
+			const task_edit_el = document.createElement('button');
+			task_edit_el.classList.add('edit');
+			task_edit_el.innerText = 'Edit';
+
+			// Delete button
+			const task_delete_el = document.createElement('button');
+			task_delete_el.classList.add('delete');
+			task_delete_el.innerText = 'Delete';
+
+			task_actions_el.appendChild(task_edit_el);
+			task_actions_el.appendChild(task_delete_el);
+
+			task_el.appendChild(task_actions_el);
+
+
+			list_el.appendChild(task_el);
+
+
+			task_check.addEventListener('click', (e) => {
+				if (task_check.checked) {
+					task_input_el.style.setProperty("text-decoration", "line-through");
+					task_input_el.style.setProperty("color", "#6B7280");
+				}
+				if (task_check.checked === false) {
+					task_input_el.style.setProperty("text-decoration", "none")
+					task_input_el.style.setProperty("color", "#EEE");
+				}
+			});
+
+			//------------------- Event for edit button functionality------------------//
+
+			task_edit_el.addEventListener('click', (e) => {
+				if (task_edit_el.innerText.toLowerCase() === "edit") {
+					task_edit_el.innerText = "Save";
+					task_input_el.removeAttribute("readonly");
+					task_input_el.focus();
+				} else {
+					task_edit_el.innerText = "Edit";
+					task_input_el.setAttribute("readonly", "readonly");
+				}
+			});
+
+			//--------------------- Event for delete button-------------------------------//
+
+			task_delete_el.addEventListener('click', (e) => {
+				list_el.removeChild(task_el);
+				localStorage.removeItem(task_input_el.value);
+			});
+		}
+	}
+	if (!(localStorage > 0)) {
 		const task_el = document.createElement('div');
 		task_el.classList.add('task');
-
-		// Checkmark for each task
-		const task_check = document.createElement('input');
-		task_check.type = 'checkbox'
 
 		const task_content_el = document.createElement('div');
 		task_content_el.classList.add('content');
 
-		task_el.appendChild(task_check);
 		task_el.appendChild(task_content_el);
 
-		// Text box used to display a task, can be edited after clicking edit
 		const task_input_el = document.createElement('input');
 		task_input_el.classList.add('text');
 		task_input_el.type = 'text';
-		task_input_el.value = arrayOfTodos[i];
+		task_input_el.value = "Would you look at that, you have nothing to do!";
 		task_input_el.setAttribute('readonly', 'readonly');
 
 		task_content_el.appendChild(task_input_el);
-		
-		// Container for edit and delete buttons
-		const task_actions_el = document.createElement('div');
-		task_actions_el.classList.add('actions');
-
-		// Edit button
-		const task_edit_el = document.createElement('button');
-		task_edit_el.classList.add('edit');
-		task_edit_el.innerText = 'Edit';
-
-		// Delete button
-		const task_delete_el = document.createElement('button');
-		task_delete_el.classList.add('delete');
-		task_delete_el.innerText = 'Delete';
-
-		task_actions_el.appendChild(task_edit_el);
-		task_actions_el.appendChild(task_delete_el);
-
-		task_el.appendChild(task_actions_el);
-		
-	
 		list_el.appendChild(task_el);
-
-
-		task_check.addEventListener('click', (e) => {
-			if (task_check.checked) {
-				task_input_el.style.setProperty("text-decoration", "line-through");
-				task_input_el.style.setProperty("color", "#6B7280");
-			}
-			if (task_check.checked === false) {
-				task_input_el.style.setProperty("text-decoration", "none")
-				task_input_el.style.setProperty("color", "#EEE");
-			}
-		});
-	
-		//------------------- Event for edit button functionality------------------//
-	
-		task_edit_el.addEventListener('click', (e) => {
-			if (task_edit_el.innerText.toLowerCase() === "edit") {
-				task_edit_el.innerText = "Save";
-				task_input_el.removeAttribute("readonly");
-				task_input_el.focus();
-			} else {
-				task_edit_el.innerText = "Edit";
-				task_input_el.setAttribute("readonly", "readonly");
-			}
-		});
-	
-		//--------------------- Event for delete button-------------------------------//
-	
-		task_delete_el.addEventListener('click', (e) => {
-			list_el.removeChild(task_el);
-			localStorage.removeItem(task_input_el.value);
-		});
 	}
 /**********************************************************************************************************************/
 
